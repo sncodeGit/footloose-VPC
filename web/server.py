4c1328cae57d961da64b0db48bbafb1576557862
Денис Докиенко <dokienko@selectel.ru>
2020-11-16 18:29:04 +0300

    web: fix serv ip and port

--- web/server.py
+++ web/server.py
@@ -1,4 +1,4 @@
 from http.server import HTTPServer, CGIHTTPRequestHandler
-server_address = ("", 8000)
+server_address = ("188.68.219.27", 80)
 httpd = HTTPServer(server_address, CGIHTTPRequestHandler)
 httpd.serve_forever()
