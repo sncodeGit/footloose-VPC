e0a935bf05ee016c2bda802326f6b79e23ccc2ef
Денис Докиенко <dokienko@selectel.ru>
2020-12-12 21:39:15 +0300

    Update

--- flask/scripts/config.py
+++ flask/scripts/config.py
@@ -1,4 +1,4 @@
 DIR_PATH = {
     'clusters_dir': '/tmp',
-    'templates': '/Users/inter000/tmp/footloose-VPC/scripts/templates'
+    'templates': '/Users/inter000/tmp/footloose-VPC/flask/scripts/templates'
 }
