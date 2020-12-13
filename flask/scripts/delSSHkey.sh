baa718e1ebb6afcbc825b333dd72b5a7d5d5d4b2
Денис Докиенко <dokienko@selectel.ru>
2020-12-13 19:17:12 +0300

    Update

--- flask/scripts/delSSHkey.sh
+++ flask/scripts/delSSHkey.sh
@@ -11,6 +11,6 @@ CLUSTER_NAME=$(cat "$1".yaml | grep name | head -1 | awk '{print $2}')
 for node in $(footloose -c "$1".yaml show | awk '{print $1}' | sed '1d')
 do
 NODE_NAME=$(echo $node | sed -r "s/$CLUSTER_NAME-//")
-KEY_STR_NUM=$(footloose -c "$1".yaml ssh $NODE_NAME "cat ~/.ssh/authorized_keys | grep -n $2" 2> /dev/null | cut -d ':' -f 1)
+KEY_STR_NUM=$(footloose -c "$1".yaml ssh $NODE_NAME "cat ~/.ssh/authorized_keys | grep -n '$2'" 2> /dev/null | cut -d ':' -f 1)
 footloose -c "$1".yaml ssh $NODE_NAME "sed -i "${KEY_STR_NUM}d" ~/.ssh/authorized_keys"
 done
