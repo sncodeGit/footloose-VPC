025faf27f6a0dc9a1f5088a37b0890e3557cf171
Денис Докиенко <dokienko@selectel.ru>
2021-05-02 22:13:07 +0300

    Fix cluster stop functionality

--- flask/scripts/stopCluster.sh
+++ flask/scripts/stopCluster.sh
@@ -7,4 +7,15 @@
 source /usr/lib/footloose-vpc/scripts/config.sh
 
 cd "$CLUSTER_DIR"
-footloose -c "$1".yaml stop
+
+CLUSTER_NAME=$(yq e ".cluster.name" "$1".yaml)
+NODE_NAME=$(yq e ".machines" "$1".yaml | grep name | awk {'print $2'})
+NODE_NAME="${CLUSTER_NAME}-${NODE_NAME}"
+MACHINES_COUNT=$(yq e ".machines" "$1.yaml" | grep count | awk {'print $3'})
+for ((i=0; i < ${MACHINES_COUNT}; i++))
+do
+    NOW_NODE=$(echo "${NODE_NAME//%d/${i}}")
+    ignite stop "$NOW_NODE"
+done
+echo "${NODE_NAME//%d/}"
+
